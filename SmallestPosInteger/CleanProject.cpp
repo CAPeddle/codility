ee0a67fc4450d1f69fda340558cbe3b212a61b77
cpeddle <chrisapeddle@gmail.com>
2024-10-17 15:18:58 +0200

    optimize smallest pointer

--- SmallestPosInteger/CleanProject.cpp
+++ SmallestPosInteger/CleanProject.cpp
@@ -4,20 +4,13 @@
 #include <algorithm>
 
 int solution(std::vector<int>& A) {
-  // The following std::sort() is crucial to the algorithm!
   std::sort(A.begin(), A.end());
-  auto iter = std::lower_bound(A.begin(), A.end(), 1);
   int smallestIntNotFound = 1;
 
-  while (iter != A.end()) {
-    if (*iter == smallestIntNotFound) {
-      // The list *does* have <smallestIntNotFound>,
-      // skip all occurrences and look for next
-      // integer value
-      while ((++iter != A.end()) && (*iter == smallestIntNotFound));
+  for (int num : A) {
+    if (num == smallestIntNotFound) {
       ++smallestIntNotFound;
-    } else {
-      // <smallestIntNotFound> was *not* found
+    } else if (num > smallestIntNotFound) {
       break;
     }
   }
