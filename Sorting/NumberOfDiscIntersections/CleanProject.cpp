d6eede2ce38ac65c31d5311ea7ac7f071133c1b7
Christopher Peddle <christopher.peddle@asml.com>
2024-11-08 20:22:23 +0100

    add numberof

---
+++ Sorting/NumberOfDiscIntersections/CleanProject.cpp
@@ -0,0 +1,15 @@
+// CleanProject.cpp
+
+#include "CleanProject.h"
+
+#include <algorithm>
+#include <vector>
+
+int solution(std::vector<int>& A)
+{
+  int result;
+
+  result = 1;
+
+  return result;
+}
