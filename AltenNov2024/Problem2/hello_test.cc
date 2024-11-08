d233a1be5da7ed8c3652cca25af5b8f26cbfe7ae
cpeddle <chrisapeddle@gmail.com>
2024-11-08 22:21:06 +0100

    Add Alten Framework

---
+++ AltenNov2024/Problem2/hello_test.cc
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+
+// Demonstrate some basic assertions.
+TEST(AltenProb2, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+// test.cpp
+#include "CleanProject.h"
+// #include "pch.h"
+
+TEST(AltenProb2, DISABLED_TestCleanProject_TestSol_Test1) {
+  std::vector<int> A = {3, 8, 9, 7, 6};
+  std::vector<int> result = solution(A, 3);
+  std::vector<int> expected = {9, 7, 6, 3, 8};
+  EXPECT_EQ(result, expected);
+}
